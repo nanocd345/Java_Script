7e7f110ae6373711ae4160c40d9d8fb2c1bb166b
CampusLands <139265002+CampusLands@users.noreply.github.com>
2024-04-18 15:50:31 -0500

    feat: :tada: Configuracion de json-server
    feat: :sparkles: Peticion get, post en js
    
    feat: :card_file_box: Datos a quemar
    
    feat: :wrench: Se cambiaron los nombres de ls json, y se creo los js de cada uno
    
    feat: :construction: Ejercicio 1,2,3,4
    
    docs: :memo: Consultas a realizar

---
+++ js/main.js
@@ -0,0 +1,10 @@
+import { 
+    getAllOficceAndCodeCity, 
+    getAllOficceCityAndMovil 
+} from "./module/offices.js";
+import { 
+    getAllFullNameAndEmailsAndBoss,
+    getBossFullNameAndEmail 
+} from "./module/employees.js";
+
+console.log(await getBossFullNameAndEmail());
